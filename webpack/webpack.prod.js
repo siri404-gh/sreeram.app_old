b24827c81c85d50cf03184f75ace4a58f3acf18a
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-07-31 23:29:05 +0100

    feat: adds service worker cacheing

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,16 @@
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
 });
