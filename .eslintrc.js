8dd075892a4e335da1852886ea93061b225e1d44
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-07-31 17:52:30 +0100

    Adds eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ["eslint:recommended", "google", "plugin:react/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  rules: {
+    "max-len": ["error", { "code": 100 }],
+    "arrow-parens": ["error", "as-needed"],
+    "require-jsdoc": [
+      "error", {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true
+        }
+      }
+    ],
+    "object-curly-spacing": ["error", "never"]
+  }
+};
