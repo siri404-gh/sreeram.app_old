34706e3d8934768c3694ebee463ac13dcd257a60
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-07-31 21:40:50 +0100

    Adds webpack-dev-server

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,6 +1,13 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    compress: true,
+    port: 9000,
+  },
 });
