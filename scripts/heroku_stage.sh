c85fbce9178a54a8b6f0e808065a330588ceceed
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-08-01 19:56:35 +0100

    fix: fixes heroku deploy script

--- scripts/heroku_stage.sh
+++ scripts/heroku_stage.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 # heroku plugins:install heroku-container-registry
-# docker login -u _ -p $HEROKU_API_KEY registry.heroku.com
+docker login -u _ -p $HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
