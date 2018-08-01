b2c871f3ee78ab69578a446e1b9078bdb845e8a1
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-08-01 06:24:53 +0100

    fix: lint fixes

--- src/web/index.js
+++ src/web/index.js
@@ -9,10 +9,10 @@ if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
     navigator.serviceWorker
       .register('/service-worker.js')
       .then(registration => {
-        console.log('SW registered: ', registration);
+        // console.log('SW registered: ', registration);
       })
       .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
+        // console.log('SW registration failed: ', registrationError);
       });
   });
 }
