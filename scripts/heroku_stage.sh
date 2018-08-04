a08c57ba5c62c724f264dde1762990899f3d237d
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-08-04 23:14:09 +0100

    refactor: refactoring travis yml

--- scripts/heroku_stage.sh
+++ scripts/heroku_stage.sh
@@ -2,5 +2,5 @@
 # wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 # heroku plugins:install heroku-container-registry
 docker login -u _ -p $HEROKU_API_KEY registry.heroku.com
-./node_modules/.bin/heroku container:push web --recursive --app $HEROKU_APP_NAME
-./node_modules/.bin/heroku container:release web --app $HEROKU_APP_NAME
+heroku container:push web --recursive --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
