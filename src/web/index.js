9ea0ddc4abad03915413e9389164697e0d26ca4a
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-08-07 00:56:26 +0100

    fix: removes commented out code

--- src/web/index.js
+++ src/web/index.js
@@ -8,11 +8,7 @@ if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/service-worker.js')
-      .then(registration => {
-        // console.log('SW registered: ', registration);
-      })
-      .catch(registrationError => {
-        // console.log('SW registration failed: ', registrationError);
-      });
+      .then(registration => {})
+      .catch(registrationError => {});
   });
 }
