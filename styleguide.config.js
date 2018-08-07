71971ded01d2c113635599c82d5d2164311662a1
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-08-07 22:11:54 +0100

    fix: fixes react-styleguide issue

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,6 +2,12 @@ module.exports = {
   components: 'src/web/components/**/[A-Z]*.js',
   webpackConfig: Object.assign({}, require('./webpack/webpack.prod.js'), {
     /* Custom config options */
+    node: {
+      net: 'empty',
+      tls: 'empty',
+      dns: 'empty',
+      fs: 'empty',
+    },
     plugins: [],
   }),
 };
