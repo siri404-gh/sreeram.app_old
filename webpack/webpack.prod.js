2d10b1302fe5fbc929c4b4ff0ead0ab9e5f6c262
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-08-07 11:31:04 +0100

    feat: removes webpack-bundle-analyzer and adds webpack-visualizer

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,18 +1,15 @@
 const merge = require('webpack-merge');
-const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const CompressionPlugin = require('compression-webpack-plugin');
+const Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-      analyzerMode: 'static',
-      reportFilename: path.resolve(__dirname, `../dist/bundle.html`),
+    new Visualizer({
+      filename: './bundle.html',
     }),
     new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
