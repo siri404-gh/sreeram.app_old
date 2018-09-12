6fb93157f63ec709034ae46ffbf2b54d27641cad
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-09-12 13:56:41 +0100

    chore: update to heading and keywords

--- config/variables.js
+++ config/variables.js
@@ -1,16 +1,16 @@
 module.exports = {
   navbar: {
-    title: 'Web Boilerplate Project',
+    title: 'The Web Boilerplate Project',
     tagline: 'Web development, supercharged!',
     tabs: [],
   },
   defaultTopic: 'home',
   defaultPost: 'home',
-  keywords: 'web development, boilerplate project, webpack, react boilerplate, web development from scratch, javascript, how to create a website from scratch, performance, progressive web apps',
+  keywords: 'web development, boilerplate project, webpack, react boilerplate, web development from scratch, javascript, how to create a website from scratch, performance, progressive web apps, travis ci, circle ci, lighthouse, continous integration, continous deployment, unit testing, karma, jasmine, BDD, TDD, cypress, browserstack, webdriver, selenium, github, gitlab, docker',
   ogImage: 'https://sreeram.app/logo.png',
   ogUrl: 'https://www.sreeram.app',
   ogTitle: 'Web Development Boilerplate project.',
-  ogDescription: 'Beginner to expert level steps on how to create your own performant, accessible, aesthetic website from scatch for free.',
+  ogDescription: 'Beginner to expert level tutorials on how to create your own performant, accessible, aesthetic website from scatch for free.',
   ogType: 'website',
   ogFbAppId: '297023651089707',
 };
